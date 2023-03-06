5578c9ea5230c44facc7af50d6290a811e9c0412
Justin Stitt <jstitt007@gmail.com>
2023-03-05 23:34:55 -0800

    update make script to generate docs

--- make.sh
+++ make.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
 pipreqs --force ./ # save dependencies
+echo "[x] Saved dependencies to requirements.txt"
+
+# generate docs
+cd ./src
+rm -r ./html 2> /dev/null
+rm -r ../docs 2> /dev/null
+pdoc --html app.py > /dev/null
+mv ./html ../docs
+cd ..
+
+echo "[x] Generated app.py docs"
