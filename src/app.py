ee515b7d0ebcfb70756e448f84e20446d78775bf
Justin Stitt <jstitt007@gmail.com>
2023-03-05 22:28:24 -0800

    create api endpoints

--- src/app.py
+++ src/app.py
@@ -1,8 +1,11 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Api, Resource
+from sheet import Sheet
+from typing import Literal
 
 app = Flask(__name__)
 api = Api(app)
+sheet = Sheet()
 
 
 class HelloWorld(Resource):
@@ -10,7 +13,83 @@ class HelloWorld(Resource):
         return {"hello": "world"}
 
 
+class CreateTeam(Resource):
+    def post(self):
+        args = request.args
+        team_name = args["team_name"]
+        resp = sheet.createTeam(team_name)
+        return resp
+
+
+class CreateEvent(Resource):
+    def post(self):
+        args = request.args
+        event_name = args["event_name"]
+        resp = sheet.createEvent(event_name)
+        return resp
+
+
+class GetScores(Resource):
+    def get(self, team_name):
+        scores, events_to_idx = sheet.getScores(team_name)
+        resp = {
+            event: score for event, score in zip(events_to_idx.keys(), list(scores))
+        }
+        # scores = [score for score in sheet.getScores(team_name)[0]]
+        return f"{team_name}:{resp}", 200
+
+
+class GetScore(Resource):
+    def get(self, team_name, event_name):
+        score = sheet.getScore(team_name, event_name)
+        return str(score), 200
+
+
+class SetScore(Resource):
+    def post(self):
+        args = request.args
+        team_name = args["team_name"]
+        event_name = args["event_name"]
+        score = args["score"]
+        resp = sheet.setScore(event_name, team_name, int(score))
+        return resp
+
+
+class AdjustScore(Resource):
+    def post(self):
+        args = request.args
+        team_name = args["team_name"]
+        event_name = args["event_name"]
+        delta = args["delta"]
+        resp = sheet.adjustScore(event_name, team_name, int(delta))
+        return resp
+
+
+class ChangeTeamName(Resource):
+    def post(self):
+        args = request.args
+        team_name = args["team_name"]
+        new_name = args["new_name"]
+        resp = sheet.changeTeamName(team_name, new_name)
+        return resp
+
+
+class GetScoreboard(Resource):
+    def get(self):
+        scoreboard = sheet.getScoreboard()
+        assert type(scoreboard) is not Literal[False]
+        return scoreboard.to_json(orient="records")
+
+
 api.add_resource(HelloWorld, "/")
+api.add_resource(CreateTeam, "/create_team")
+api.add_resource(CreateEvent, "/create_event")
+api.add_resource(GetScores, "/scores/<team_name>")
+api.add_resource(GetScore, "/scores/<team_name>/<event_name>")
+api.add_resource(SetScore, "/set_score")
+api.add_resource(AdjustScore, "/adjust_score")
+api.add_resource(ChangeTeamName, "/change_team_name")
+api.add_resource(GetScoreboard, "/scoreboard")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- src/sheet.py
+++ src/sheet.py
@@ -1,3 +1,4 @@
+from hashlib import new
 from typing import Literal
 
 from dotenv import load_dotenv
@@ -11,19 +12,19 @@ load_dotenv()
 
 
 class Sheet:
-    def __init__(self, client: Client, logger: Logger):
-        self._client = client
-        self._logger = logger
+    def __init__(self):
+        self._client = Client()
+        self._logger = Logger(self._client)
         # if logger and client.client != logger._client:
         #     raise Exception("Client and Logger not synced.")
-        self.scoreboard = self._client.scoreboard
+        self._scoreboard = self._client.scoreboard
 
     def getScoreboard(self) -> pd.DataFrame | Literal[False]:
-        return self.scoreboard.get_as_df()
+        return self._scoreboard.get_as_df()
 
     # TODO: reduce redundancy in _findEvent and _findTeam
     def _findEvent(self, event_name: str) -> ps.Cell:
-        cells = self.scoreboard.find(event_name)
+        cells = self._scoreboard.find(event_name)
         if len(cells) == 0:
             raise ps.CellNotFound(
                 f"Could not find event: {event_name}. Check spelling (case sensitive)."
@@ -31,7 +32,7 @@ class Sheet:
         return cells[0]
 
     def _findTeam(self, team_name: str) -> ps.Cell:
-        cells = self.scoreboard.find(team_name)
+        cells = self._scoreboard.find(team_name)
         if len(cells) == 0:
             raise ps.CellNotFound(
                 f"Could not find team: {team_name}. Check spelling (case sensitive)."
@@ -41,15 +42,16 @@ class Sheet:
     def changeTeamName(self, old_team_name: str, new_team_name: str):
         old_team_cell: ps.Cell = self._findTeam(old_team_name)
         old_team_cell.set_value(new_team_name)
-        old_team_cell.link(self.scoreboard, update=True)
+        old_team_cell.link(self._scoreboard, update=True)
         self._logger.log()
+        return f'Successfully changed team: "{old_team_name}" to "{new_team_name}"', 200
 
     def _getNumberOfTeams(self) -> int:
-        header_row = self.scoreboard.get_row(1, include_tailing_empty=False)
+        header_row = self._scoreboard.get_row(1, include_tailing_empty=False)
         return len(header_row) - 1
 
     def _getNumberOfEvents(self) -> int:
-        leading_col = self.scoreboard.get_col(1, include_tailing_empty=False)
+        leading_col = self._scoreboard.get_col(1, include_tailing_empty=False)
         return len(leading_col) - 1
 
     def createTeam(self, team_name: str):
@@ -58,16 +60,18 @@ class Sheet:
         except:
             idx = self._getNumberOfTeams() + 1
             zero_pad = [0 for _ in range(self._getNumberOfEvents())]
-            self.scoreboard.insert_cols(idx, values=[team_name, *zero_pad])
+            self._scoreboard.insert_cols(idx, values=[team_name, *zero_pad])
             self._logger.log()
+            return f'Team: "{team_name} created', 200
         else:
-            print(f'Team: "{team_name}" already exists!')
+            return f'Team: "{team_name}" already exists!', 304
 
     def createEvent(self, event_name: str):
         idx = self._getNumberOfEvents() + 1
         zero_pad = [0 for _ in range(self._getNumberOfTeams())]
-        self.scoreboard.insert_rows(idx, values=[event_name, *zero_pad])
+        self._scoreboard.insert_rows(idx, values=[event_name, *zero_pad])
         self._logger.log()
+        return f'Event: "{event_name}" created', 200
 
     def _getEventTeamCell(self, event_name: str, team_name: str) -> str | None:
         try:
@@ -93,28 +97,32 @@ class Sheet:
         event_to_idx = {k: v for (v, k) in events.items()}
         return sb[team_name], event_to_idx
 
+    def getScore(self, team_name: str, event_name: str) -> int:
+        team_col, event_to_idx = self.getScores(team_name)
+        score_idx = event_to_idx[event_name]
+        score = team_col[score_idx]
+        return score
+
     def setScore(self, event_name: str, team_name: str, score: int):
         label = self._getEventTeamCell(event_name, team_name)
-        self.scoreboard.update_value(label, str(score))
+        self._scoreboard.update_value(label, str(score))
         self._logger.log()
+        return "success", 200
 
     def adjustScore(self, event_name: str, team_name: str, score_delta: int):
         assert type(score_delta) is int, "Score Delta must be an integer!"
         label = self._getEventTeamCell(event_name, team_name)
-        team_col, event_to_idx = self.getScores(team_name)
-        score_idx = event_to_idx[event_name]
-        current_score = team_col[score_idx]
-        self.scoreboard.update_value(label, str(current_score + score_delta))
+        current_score = self.getScore(team_name, event_name)
+        self._scoreboard.update_value(label, str(current_score + score_delta))
         self._logger.log(old_score=current_score, new_score=current_score + score_delta)
+        return "success", 200
 
 
 if __name__ == "__main__":
-    gc = Client()
-    logger = Logger(gc)
-    sheet = Sheet(gc, logger)
+    sheet = Sheet()
     # sheet.setScore("HACKATHON", "Team Three", 99)
     # scores = sheet.getScores("Team Three")
-    sheet.adjustScore("HACKATHON", "Team Three", 18)
+    # sheet.adjustScore("AOC-4", "Team Four", 6)
     # sheet.createTeam("Team Five")
     # sheet.createEvent("Cool Event")
     # sheet.changeTeamName("Flying Felines", "V2 Coolio Team")
