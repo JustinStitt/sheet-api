0bd95aaa2bddb2fc8d5c7848f030513395d1091f
Justin Stitt <jstitt007@gmail.com>
2023-03-12 15:20:29 -0700

    dont add emptry strings to args

--- src/sanitize.py
+++ src/sanitize.py
@@ -5,9 +5,10 @@ def sanitize(func):
     def wrapper(*args, **kwargs):
         sanitized_args = []
         for arg in args:
-            if type(arg) is str and 'code' not in arg:
+            if type(arg) is str and "code" not in arg:
                 arg = re.sub(r"[^a-zA-Z]", "", arg).lower()
-            sanitized_args.append(arg)
+            if type(arg) is str and len(arg):
+                sanitized_args.append(arg)
         return func(*sanitized_args, **kwargs)
 
     return wrapper
