1834da759729df2af9cc645c8cd62a43f504f365
Justin Stitt <jstitt007@gmail.com>
2023-03-13 00:49:16 -0700

    add pass or fail to log

--- src/judge.py
+++ src/judge.py
@@ -21,7 +21,15 @@ class Judge:
         relevant_col = relevant_sheet.get_col(cidx, include_tailing_empty=False)
         correct_output = relevant_col[input_idx + 1]
         result = correct_output == output
-        row = [gettime(), team_name, problem, str(result), str(input_idx), output]
+        row = [
+            gettime(),
+            team_name,
+            problem,
+            str(result),
+            str(input_idx),
+            output,
+            "PASS" if result else "FAIL",
+        ]
         self.submissions.append_table(row, overwrite=True)  # type: ignore
         return result
 
