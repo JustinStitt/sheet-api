765fa55a8572480eda1cf63bc2aa83b9df9429de
Justin Stitt <jstitt007@gmail.com>
2023-03-13 14:26:25 -0700

    fix again

--- src/sanitize.py
+++ src/sanitize.py
@@ -8,6 +8,8 @@ def sanitize(func):
             if type(arg) is str and "woc" not in arg and "<" not in arg:
                 arg = re.sub(r"[^a-zA-Z]", "", arg).lower()
             # if type(arg) is str and "<" not in arg:
+            if type(arg) is str and "<" in arg:
+                continue
             sanitized_args.append(arg)
         return func(*sanitized_args, **kwargs)
 
